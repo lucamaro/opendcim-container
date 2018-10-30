92029bd8eb614e5815b0e54bdbcc0f42fb79e7a1
Luca Maragnani <luca.maragnani@fastweb.it>
2018-10-30 23:07:59 +0100

    Environment variables required

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 
 if [ ! -f /.configured ] ; then
-	# configure port with environment var DBHOST
-	sed -i "s/[$]dbhost = [']localhost[']/\$dbhost = '$DBHOST'/" /var/www/dcim/db.inc.php
-	sed -i "s/[$]dbname = [']dcim[']/\$dbname = '$DCIM_DB_SCHEMA'/" /var/www/dcim/db.inc.php
-	sed -i "s/[$]dbuser = [']dcim[']/\$dbuser = '$DCIM_DB_USER'/" /var/www/dcim/db.inc.php
-	sed -i "s/[$]dbpass = [']dcim[']/\$dbpass = '$DCIM_DB_PASSWD'/" /var/www/dcim/db.inc.php
+	cp /var/www/dcim/db.inc.php-dist /var/www/dcim/db.inc.php
 
 	if [ -f "$SSL_CERT_FILE" ] && [ -f "$SSL_KEY_FILE" ] ; then
 		a2enmod ssl
